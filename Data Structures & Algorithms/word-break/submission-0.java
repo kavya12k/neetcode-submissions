289c7636b209b0c41569dd4cb80b4bca11331042
kavya12k <37460399+kavya12k@users.noreply.github.com>
2026-08-20 02:37:37 -0700

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1]; // can the prefix i - n be formed?
+        dp[s.length()] = true;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (String w : wordDict) {
+                if (i + w.length() <= s.length() && s.substring(i, i + w.length()).equals(w)) {
+                    dp[i] = dp[i + w.length()];
+                }
+                if (dp[i] == true) {
+                    break;
+                }
+            }
+        }
+        return dp[0];
+    }
+}
